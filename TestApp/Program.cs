daa448f13582ba757b61f8d4206cd0a5618dbec9
Chase M <48492365+chasemieras@users.noreply.github.com>
2020-09-14 11:41:19 -0400

    UI porject added to my branch, removed testapp

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -6,7 +6,7 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("dab");
         }
     }
 }
