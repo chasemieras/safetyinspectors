1ace6e4fac163ff5730d3223b83fd77d0714a811
Nathan Bisheimer <gameplayer5250@gmail.com>
2020-09-17 17:53:21 -0400

    This is qr code taken from: https://www.youtube.com/watch?v=l7K40iIlqkw | https://blog.basseltech.com/webcam-qr-code-scanner-in-c/ looks like it works need to test more with other random qr codes, I got one on google to error and 2 of his examples to work so will have to do more research.

---
+++ QR Tester/QR Tester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QR_Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WebCam QR Code Scanner/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+using BasselTech_CamCapture;
+
+namespace WebCam_QR_Code_Scanner
+{
+    public partial class Form1 : Form
+    {
+        Camera cam;
+        Timer t;
+        BackgroundWorker worker;
+        Bitmap CapImage;
+        public Form1()
+        {
+            InitializeComponent();
+
+            t = new Timer();
+            cam = new Camera(pictureBox1);
+            worker = new BackgroundWorker();
+
+            worker.DoWork += Worker_DoWork;
+            t.Tick += T_Tick;
+            t.Interval = 1;
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            CapImage = cam.GetBitmap();
+            if (CapImage != null && !worker.IsBusy)
+                worker.RunWorkerAsync();
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            QRCodeDecoder Decoder = new QRCodeDecoder();
+
+            try
+            {
+                MessageBox.Show(Decoder.decode(new QRCodeBitmapImage(CapImage)));
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cam.Start();
+                t.Start();
+                button2.Enabled = true;
+                button1.Enabled = false;
+            }
+            catch(Exception ex)
+            {
+                cam.Stop();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t.Stop();   // I forgot this line in the video
+            cam.Stop();
+            button2.Enabled = false;
+            button1.Enabled = true;
+        }
+    }
+}
