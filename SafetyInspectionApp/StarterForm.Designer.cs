ad65abdd1d371cfe86571bfc542af8fe7a7e5749
Nathan Bisheimer <gameplayer5250@gmail.com>
2020-10-27 15:33:47 -0400

    Got the camera into the starter form.

--- SafetyInspectionApp/StarterForm.Designer.cs
+++ SafetyInspectionApp/StarterForm.Designer.cs
@@ -28,18 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.MainNextButton = new System.Windows.Forms.Button();
             this.FormSelectGroup = new System.Windows.Forms.GroupBox();
             this.LadderForm = new System.Windows.Forms.CheckBox();
+            this.txtQRCode = new System.Windows.Forms.TextBox();
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.cboCamera = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.FormSelectGroup.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(357, 175);
+            this.textBox1.Location = new System.Drawing.Point(152, 174);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(149, 23);
             this.textBox1.TabIndex = 0;
@@ -47,7 +55,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(234, 127);
+            this.label1.Location = new System.Drawing.Point(29, 126);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(351, 45);
             this.label1.TabIndex = 1;
@@ -57,7 +65,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(254, 178);
+            this.label2.Location = new System.Drawing.Point(49, 177);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(97, 15);
             this.label2.TabIndex = 2;
@@ -77,7 +85,7 @@
             // 
             this.FormSelectGroup.Controls.Add(this.LadderForm);
             this.FormSelectGroup.Controls.Add(this.MainNextButton);
-            this.FormSelectGroup.Location = new System.Drawing.Point(306, 213);
+            this.FormSelectGroup.Location = new System.Drawing.Point(101, 212);
             this.FormSelectGroup.Name = "FormSelectGroup";
             this.FormSelectGroup.Size = new System.Drawing.Size(200, 100);
             this.FormSelectGroup.TabIndex = 4;
@@ -94,11 +102,65 @@
             this.LadderForm.Text = "Ladder Inspection";
             this.LadderForm.UseVisualStyleBackColor = true;
             // 
+            // txtQRCode
+            // 
+            this.txtQRCode.Location = new System.Drawing.Point(829, 72);
+            this.txtQRCode.Multiline = true;
+            this.txtQRCode.Name = "txtQRCode";
+            this.txtQRCode.Size = new System.Drawing.Size(195, 132);
+            this.txtQRCode.TabIndex = 4;
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.Location = new System.Drawing.Point(422, 51);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(401, 378);
+            this.PictureBox.TabIndex = 3;
+            this.PictureBox.TabStop = false;
+            // 
+            // cboCamera
+            // 
+            this.cboCamera.FormattingEnabled = true;
+            this.cboCamera.Location = new System.Drawing.Point(479, 22);
+            this.cboCamera.Name = "cboCamera";
+            this.cboCamera.Size = new System.Drawing.Size(239, 23);
+            this.cboCamera.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(422, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Camera:";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(724, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnStart_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // StarterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1045, 465);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboCamera);
+            this.Controls.Add(this.PictureBox);
+            this.Controls.Add(this.txtQRCode);
             this.Controls.Add(this.FormSelectGroup);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -106,8 +168,11 @@
             this.Name = "StarterForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Safety Inspection Form";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load_1);
             this.FormSelectGroup.ResumeLayout(false);
             this.FormSelectGroup.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,5 +186,11 @@
         private System.Windows.Forms.Button MainNextButton;
         private System.Windows.Forms.GroupBox FormSelectGroup;
         private System.Windows.Forms.CheckBox LadderForm;
+        private System.Windows.Forms.TextBox txtQRCode;
+        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.ComboBox cboCamera;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- SafetyInspectionApp/StarterForm.cs
+++ SafetyInspectionApp/StarterForm.cs
@@ -5,6 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
 
 namespace SafetyInspectionApp
 {
@@ -24,6 +28,72 @@ namespace SafetyInspectionApp
             {
                 selectedForm = new LadderFormMain();
                 formHelper.setUpForm(selectedForm, this);
+            }
+        }
+
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo Device in filterInfoCollection)
+                cboCamera.Items.Add(Device.Name);
+            cboCamera.SelectedIndex = 0;
+            videoCaptureDevice = new VideoCaptureDevice();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+                cboCamera.Items.Add(filterInfo.Name);
+            cboCamera.SelectedIndex = 0;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoCaptureDevice.IsRunning)
+                videoCaptureDevice.SignalToStop();
+        }
+
+        private void btnStart_Click_1(object sender, EventArgs e)
+        {
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cboCamera.SelectedIndex].MonikerString);
+            videoCaptureDevice.NewFrame += CaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
+            timer1.Start();
+        }
+
+        private void CaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            PictureBox.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (PictureBox.Image != null)
+            {
+                BarcodeReader barcodeReader = new BarcodeReader();
+                Result result = barcodeReader.Decode((Bitmap)PictureBox.Image);
+                if (result != null)
+                {
+                    if(result.ToString().Equals("1"))
+                    {
+                        Form selectedForm;
+                        selectedForm = new LadderFormMain();
+                        formHelper.setUpForm(selectedForm, this);
+                    }
+                    txtQRCode.Text = result.ToString();
+                    timer1.Stop();
+                    if (videoCaptureDevice.IsRunning)
+                        videoCaptureDevice.SignalToStop();
+                }
             }
         }
     }
